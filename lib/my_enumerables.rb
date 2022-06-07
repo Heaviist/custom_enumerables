806d56c8a9ded98dc7d501a7a2645f7f055a47e9
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-06-07 16:20:28 +0200

    Add all other modules
    Once you've got the hang of it, it aall kind of makes sense. Just use the yield to execute whatever the block is on the content and then process that result into whatever you need done to it.
    
    Overall quite happy with the relatively straightforward solutions.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -22,6 +22,62 @@ module Enumerable
     selection
   end
 
+  def my_all?
+    return to_enum unless block_given?
+
+    for element in self
+      return false if yield(element) == false
+    end
+    true
+  end
+
+  def my_any?
+    return to_enum unless block_given?
+
+    for element in self
+      return true if yield(element) == true
+    end
+    false
+  end
+
+  def my_none?
+    return to_enum unless block_given?
+
+    for element in self
+      return false if yield(element) == true
+    end
+    true
+  end
+
+  def my_count
+    return size unless block_given?
+
+    count = 0
+    for element in self
+      count += 1 if yield(element) == true
+    end
+    count
+  end
+
+  def my_map
+    return to_enum unless block_given?
+
+    map = []
+    for element in self
+      map << yield(element)
+    end
+    map
+  end
+
+  def my_inject(initial_value)
+    return to_enum unless block_given?
+
+    value = initial_value
+    for element in self
+      value = yield(element, value)
+    end
+    value
+  end
 end
 
 # Custom each method for arrays
